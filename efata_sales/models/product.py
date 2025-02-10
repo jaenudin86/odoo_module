d8c894caee8d422f94a2620c3f72e31313b7332d
jaenudin <jaenudin@voksel.co.id>
2025-02-10 13:54:18 +0700

    modified:   efata_sales/models/product.py

--- efata_sales/models/product.py
+++ efata_sales/models/product.py
@@ -3,10 +3,16 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    @api.depends('list_price')
-    def _compute_standard_price(self):
+    # @api.depends('list_price')
+    # def _compute_standard_price(self):
+    #     for product in self:
+    #         if product.list_price:
+    #             product.standard_price = product.list_price * 0.8
+    
+    @api.depends('standard_price')
+    def _compute_list_price(self):
         for product in self:
-            if product.list_price:
-                product.standard_price = product.list_price * 0.8
+            if product.standard_price:
+                product.list_price = product.standard_price * 0.8
 
     # standard_price = fields.Float(compute='_compute_standard_price', store=True, string='Cost')
