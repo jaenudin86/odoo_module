d8784b8f5bf1167b04cab0c9fd0ab380ddc8f81b
jaenudin <jaenudin@voksel.co.id>
2025-02-10 13:36:15 +0700

    modified:   efata_sales/__manifest__.py 	modified:   efata_sales/__pycache__/__init__.cpython-312.pyc 	modified:   efata_sales/controllers/__pycache__/__init__.cpython-312.pyc 	modified:   efata_sales/controllers/__pycache__/controllers.cpython-312.pyc 	modified:   efata_sales/models/__init__.py 	modified:   efata_sales/models/__pycache__/__init__.cpython-312.pyc 	modified:   efata_sales/models/__pycache__/models.cpython-312.pyc 	new file:   efata_sales/models/__pycache__/product.cpython-312.pyc 	modified:   efata_sales/models/__pycache__/sale_order.cpython-312.pyc 	modified:   efata_sales/models/product.py 	new file:   efata_sales/views/report_saleorder.xml

--- efata_sales/models/product.py
+++ efata_sales/models/product.py
@@ -3,9 +3,10 @@ from odoo import models, fields, api
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    @api.onchange('list_price')
-    def _onchange_list_price(self):
-        """ Mengubah Cost secara otomatis saat Sales Price diubah """
+    @api.depends('list_price')
+    def _compute_standard_price(self):
         for product in self:
             if product.list_price:
                 product.standard_price = product.list_price * 0.8
+
+    # standard_price = fields.Float(compute='_compute_standard_price', store=True, string='Cost')
