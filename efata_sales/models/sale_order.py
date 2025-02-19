64523677dffc9e0b8377e10e9079aab2e5ff8c1e
jaenudin <jaenudin@voksel.co.id>
2025-02-19 17:19:17 +0700

    sales

--- efata_sales/models/sale_order.py
+++ efata_sales/models/sale_order.py
@@ -1,10 +1,22 @@
 from odoo import models, fields, api, exceptions
-
+import logging
 class InheritSaleOrder(models.Model):
     _inherit = "sale.order"
+    _logger = logging.getLogger(__name__)
 
     state = fields.Selection(selection_add=[('sample_sent', 'Sample Sent')], ondelete={'sample_sent': 'set default'})
+    report_type = fields.Selection([
+        ('quotation', 'Quotation'),
+        ('submission', 'Submission')
+    ], string="Report Type", default='quotation')
 
+    incl_tax = fields.Boolean(string="Incl. Tax", default=False)
+    @api.onchange('incl_tax')
+    def _compute_incl_tax(self):
+        self._logger.info("=== _compute_price_unit EXECUTED ===")
+        for order in self:
+            for line in order.order_line:
+                line._compute_price_with_tax() # Ensure price unit change is handled
     type_transaksi = fields.Selection([
         ('so', 'Sales Order'),
         ('sample', 'Sample Order')
@@ -33,3 +45,17 @@ class InheritSaleOrder(models.Model):
         for order in self.filtered(lambda r: r.state == 'sale'):  # 🔹 Hanya order yang sudah dikonfirmasi
             if order.type_transaksi == 'sample':
                 order.write({'state': 'sample_sent'})  # 🔹 Ubah state setelah action_confirm()
+        
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+    tax_amount = fields.Monetary(string="Pajak", compute="_compute_tax_amount", store=True)
+
+    @api.depends('product_id', 'tax_id', 'price_unit', 'order_id.incl_tax')
+    def _compute_price_with_tax(self):
+        """Menghitung pajak hanya jika checkbox aktif"""
+        for line in self:
+            if line.order_id.incl_tax:
+                tax_rate = sum(line.tax_id.mapped('amount')) / 100
+                line.tax_amount = line.price_unit * tax_rate
+            else:
+                line.tax_amount = 0.0
