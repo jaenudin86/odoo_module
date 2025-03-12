e039e059044e898a2fd2175768d8a5d3a0923fb3
jaenudin <jaenudin@voksel.co.id>
2025-03-12 10:10:48 +0700

    update

--- efata_sales/models/product.py
+++ efata_sales/models/product.py
@@ -14,3 +14,34 @@ class ProductTemplate(models.Model):
             for product in self:
                     product.list_price = product.standard_price / 0.75
     list_price = fields.Float(compute='_compute_list_price',  readonly=False)
+
+
+class ProductProduct(models.Model):
+        _inherit = "product.template"
+
+        def name_get(self):
+                result = []
+                for product in self:
+                 reference = product.default_code or ""
+                name = product.reference2 or ""
+                barcode = product.name or ""
+                full_name = f"[{reference}] {name} - {barcode}" if name else f"{reference} - {barcode}"
+                result.append((product.id, full_name))
+                return result
+
+        @api.model
+        def name_search(self, name='', args=None, operator='ilike', limit=100):
+                args = args or []
+                if name:
+                        domain = ['|', '|',
+                                ('default_code', operator, name),
+                                ('reference2', operator, name),
+                                ('barcode', operator, name)]
+                else:
+                        domain = []
+
+                products = self.search(domain + args, limit=limit)
+                if not products:
+                        return []
+                return products.name_get()
+
