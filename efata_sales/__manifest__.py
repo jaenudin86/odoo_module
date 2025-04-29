4cacbecf619375dfa7b0e9561bdb020b2cc334d2
jaenudin <jaenudin@voksel.co.id>
2025-04-29 17:34:37 +0700

    09

--- efata_sales/__manifest__.py
+++ efata_sales/__manifest__.py
@@ -37,6 +37,7 @@ Long description of module's purpose
         'views/stock_report_list_view.xml',
         'views/product_product_inherit.xml',
         'views/view_partner_form_inherit_user_visible.xml',
+        'views/hide_vendor.xml',
         # 'security/groups.xml'
     ],
     'assets': {
